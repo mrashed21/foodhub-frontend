e3f64cfdb04feef9b4fe6cee388ba62040b35ea3
mrashed21 <rashedjaman768@gmail.com>
2026-02-02 03:14:08 +0600

    update

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/auth/:path*",
-        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/:path*`,
+        destination: "https://foodhub-backend-pearl.vercel.app/api/auth/:path*",
       },
     ];
   },
