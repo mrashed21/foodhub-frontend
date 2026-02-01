3ac2ef02e94bb833f88953961510b8858cf8b9ed
mrashed21 <rashedjaman768@gmail.com>
2026-02-02 00:09:13 +0600

    configure foor host

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,14 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/auth/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
