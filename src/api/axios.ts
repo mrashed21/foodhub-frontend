3b2c51b60b120d1670e9d957cf3efe69d46a52cc
mrashed21 <rashedjaman768@gmail.com>
2026-02-02 19:58:56 +0600

    update

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,3 +1,20 @@
+// import { env } from "@/env";
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "https://foodhub-backend-pearl.vercel.app/api/v1",
+//   withCredentials: true,
+// });
+
+// api.interceptors.response.use(
+//   (res) => res,
+//   (error) => {
+//     return Promise.reject(error.response?.data || error.message);
+//   },
+// );
+
+// export default api;
+
 import axios from "axios";
 
 const api = axios.create({
@@ -5,11 +22,27 @@ const api = axios.create({
   withCredentials: true,
 });
 
+// 🔑 REQUEST INTERCEPTOR (IMPORTANT)
+api.interceptors.request.use(async (config) => {
+  // frontend server route
+  const res = await fetch("/api/auth/get-session", {
+    credentials: "include",
+  });
+
+  if (res.ok) {
+    const data = await res.json();
+
+    if (data?.session?.token) {
+      config.headers.Authorization = `Bearer ${data.session.token}`;
+    }
+  }
+
+  return config;
+});
+
 api.interceptors.response.use(
   (res) => res,
-  (error) => {
-    return Promise.reject(error.response?.data || error.message);
-  },
+  (error) => Promise.reject(error.response?.data || error.message),
 );
 
 export default api;
