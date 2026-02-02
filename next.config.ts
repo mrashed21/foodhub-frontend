ddb281f5ee635b08e12156fd73b31680901b96d6
mrashed21 <rashedjaman768@gmail.com>
2026-02-02 23:06:01 +0600

    add readme

--- next.config.ts
+++ next.config.ts
@@ -7,22 +7,15 @@ const nextConfig: NextConfig = {
 
   async rewrites() {
     return [
-      // Auth routes (Better Auth)
       {
         source: "/api/auth/:path*",
         destination:
-          process.env.NODE_ENV === "production"
-            ? "https://backend-foodhub-mrashed21.vercel.app/api/auth/:path*"
-            : "http://localhost:5000/api/auth/:path*",
+          "https://backend-foodhub-mrashed21.vercel.app/api/auth/:path*",
       },
-
-      // API v1 routes
       {
         source: "/api/v1/:path*",
         destination:
-          process.env.NODE_ENV === "production"
-            ? "https://backend-foodhub-mrashed21.vercel.app/api/v1/:path*"
-            : "http://localhost:5000/api/v1/:path*",
+          "https://backend-foodhub-mrashed21.vercel.app/api/v1/:path*",
       },
     ];
   },
