f4e186e80aeb238e0210675cff64a6cda133ade0
mrashed21 <rashedjaman768@gmail.com>
2026-02-24 02:37:54 +0600

    update service

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
   baseURL: "https://backend-foodhub-mrashed21.vercel.app",
+  fetchOptions: {
+    credentials: "include",
+  },
 });
