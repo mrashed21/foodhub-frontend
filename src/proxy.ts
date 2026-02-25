d74180f337625b0b6aa772475ca21c0f89201437
mrashed21 <rashedjaman768@gmail.com>
2026-02-25 10:58:49 +0600

    update proxy

---
+++ src/proxy.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function proxy(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (
+    pathname.startsWith("/auth/login") ||
+    pathname.startsWith("/auth/register") ||
+    pathname.startsWith("/auth/verify-email")
+  ) {
+    return NextResponse.next();
+  }
+
+  const sessionToken =
+    request.cookies.get("better-auth.session_token")?.value ||
+    request.cookies.get("__Secure-better-auth.session_token")?.value;
+
+  if (!sessionToken) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/cart",
+    "/checkout",
+    "/user/:path*",
+    "/vendor/:path*",
+    "/admin/:path*",
+  ],
+};
